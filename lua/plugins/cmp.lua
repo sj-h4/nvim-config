cda9c603f96a8550ce2606d327b0f7608a43150c
sj <64534668+sj-h4@users.noreply.github.com>
2024-05-20 10:24:12 +0900

    Update cmp settigns to use spertab;

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -24,6 +24,15 @@ return {
       local luasnip = require("luasnip")
       local cmp = require("cmp")
 
+      opts.preselect = cmp.PreselectMode.None
+      opts.completion = {
+        completeopt = "noselect",
+      }
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<S-CR>"] = cmp.mapping.confirm({ select = true }),
+      })
+
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
